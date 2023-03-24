e3e1640aaed84c1b529eb7d6b94ecf7051ca48f5
Jake Egbert <jake.egbert04@gmail.com>
2023-03-23 18:13:05 -0600

    figured out buttons

--- script.js
+++ script.js
@@ -8,16 +8,18 @@ function fetchUrl() {
     console.error(error)
   }
 }
-console.log(fetchUrl())
+// console.log(fetchUrl())
 
+const userArray = []
 function usersArray(user) {
-  const userArray = []
-  for(let i = 0; i < user.length; i++) {
-    userArray.push(user)
-  
-  }
-  console.log(userArray)
+  userArray.push(user)
+  // console.log(userArray)
 }
+
+// const data = fetch("MOCK_DATA.json", {})
+// .then(res => res.json())
+// // .then(data =>  console.log(data))
+// .catch(err => console.log(err))
 
 
 
@@ -26,13 +28,13 @@ function capsName(firstName, lastName) {
 }
 
 function addToPage(fullName) {
+  const nameNode = document.createTextNode(`${fullName} : `)
+  const weightNum = document.createTextNode("1")
   const users = document.getElementById("users-wrapper")
   const nameDiv = document.createElement("div")
   const buttonDiv = document.createElement("div")
   const namePara = document.createElement("p")
   const weightSpan = document.createElement("span")
-  const nameNode = document.createTextNode(`${fullName} : `)
-  const weightNum = document.createTextNode("1")
   const add = document.createTextNode("+")
   const minus = document.createTextNode("-")
   const buttonAdd = document.createElement("button")
@@ -44,6 +46,7 @@ function addToPage(fullName) {
   namePara.appendChild(nameNode)
   namePara.append(weightSpan)
   weightSpan.appendChild(weightNum)
+  weightSpan.classList.add("user-weight")
   nameDiv.classList.add("all-users")
   users.appendChild(nameDiv)
 
@@ -53,46 +56,62 @@ function addToPage(fullName) {
   buttonDiv.appendChild(buttonAdd)
   buttonDiv.appendChild(buttonMinus)
   buttonAdd.appendChild(add)
+  buttonAdd.classList.add("click-add")
+  // buttonAdd.onclick = update
   buttonMinus.appendChild(minus)
   users.appendChild(nameDiv)
-
+  
+  
+  // var updateButton = buttonAdd.addEventListener("click", function(e) {
+    //   e.target.parentElement.parentElement.firstChild.firstElementChild.innerText++
+    // })
+    
+  }
+  
+  function weight(user) {
+  const userWeight = document.querySelector("span")
+  userWeight.addEventListener("click", function(e) {
+    
+  })
+  user["weight"] = userWeight
+  console.log(user)
+    const updateWeight = user.weight = user.weight ++
 }
 
-// function usersArray(users){
-//   const usersArray = []
-//   for(const i in users) {
-//     usersArray.push(users)
-//   }
-//   console.log(usersArray)
-// }
 
-
-// seperateNames()
-// Focus on driving logic
-//
-
-async function main() {
-  const data = await fetchUrl()
-  // const userFrag = new DocumentFragment();
-
-  data.forEach(user => {
+function seperateName(user) {
     // createElements
     // create text nodes
-    const nameNode = (`${capsName(user.first_name, user.last_name)}`)
-    // console.log(nameNode)
-
-    addToPage(nameNode)
     
-    // userFrag.appendChild(nameNode)
-    usersArray(nameNode)
-    // return nameNode
-  })
+    const nameNode = (`${capsName(user.first_name, user.last_name)}`)
+    console.log(nameNode)
+    
+    // addClick.addEventListener("click", updateWeight)
+    
+    addToPage(nameNode)
+}
 
+async function main() {
+    const data = await fetchUrl()
+    data.forEach(user => {
+      seperateName(user)
+      weight(user)
+    })
+
+    // const userFrag = new DocumentFragment();
+    // console.log(data)
+
+    // userFrag.appendChild(nameNode)
+    // usersArray(nameNode)
+    // return nameNode
+  
+    return data
   // parentEl.appendChild(userFrag)
 }
 
 main()
 
+// console.log(document.getElementsByClassName())
 
 // async main() -> control flow of app
  // getData()
